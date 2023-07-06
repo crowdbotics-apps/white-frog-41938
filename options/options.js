7b7dea640e794625ffac131e0a24e8c3c3b26e98
Crowdbotics <team@crowdbotics.com>
2023-07-06 12:26:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-frog-41938",
+  url: "https://white-frog-41938.botics.co",
+  api: "https://white-frog-41938.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
